29bc07d3fb5d491a2e6d3c9191cb1ae97a205de2
Jacob <griff219@mymail.tsc.fl.edu>
2026-04-26 19:01:17 -0400

    Moses Player Movement

--- player.gd
+++ player.gd
@@ -1,17 +1,38 @@
+
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed: float = 220.0
 
-var target
+var target_position: Vector2
+var moving := false
 
 func _ready():
-	target = global_position
+	target_position = global_position
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		target = get_global_mouse_position()
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var space_state = get_world_2d().direct_space_state
+		
+		# Cast ray from mouse click
+		var query = PhysicsPointQueryParameters2D.new()
+		query.position = get_global_mouse_position()
+		query.collide_with_areas = true
+		query.collide_with_bodies = true
+		
+		var result = space_state.intersect_point(query)
+		
+		# If clicked on a collider
+		if result.size() > 0:
+			target_position = get_global_mouse_position()
+			moving = true
 
-func _physics_process(_delta):
-	velocity = global_position.direction_to(target) * speed
-	if global_position.distance_to(target) > 10:
-		move_and_slide()
+func _physics_process(delta):
+	if moving:
+		var direction = target_position - global_position
+		
+		if direction.length() > 5:
+			velocity = direction.normalized() * speed
+			move_and_slide()
+		else:
+			velocity = Vector2.ZERO
+			moving = false
